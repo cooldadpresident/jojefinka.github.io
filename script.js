06c04808d89492e03b72df0804261a1f163f1f1a
cooldadpresident <bumabuch@gmail.com>
2024-03-29 14:00:18 +0100

    new repo, ascii flower

---
+++ script.js
@@ -0,0 +1,17 @@
+const textElement = document.getElementById("KytickaText");
+const textWidth = textElement.offsetWidth;
+const textHeight = textElement.offsetHeight;
+
+console.log(textWidth, textHeight);
+
+alert("Miluju te bobicku");
+function scrollToBottom() {
+    const documentHeight = document.documentElement.scrollHeight;
+    window.scrollTo({ top: documentHeight, behavior: 'smooth' });
+  }
+  
+  // Call the function to initiate scrolling (optional placement)
+  scrollToBottom();
+  // Alternatively, call the function on an event (e.g., button click)
+  // document.getElementById('scrollToBottomButton').addEventListener('click', scrollToBottom);
+
